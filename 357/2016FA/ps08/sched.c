f7aac40ad7088ad972b0019139094288b200d73c
Brian Hong <brian@hongs.me>
2016-12-17 18:06:16 -0500

    [357] Implemented sched_init

--- 357/2016FA/ps08/sched.c
+++ 357/2016FA/ps08/sched.c
@@ -1,20 +1,108 @@
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include "jmpbuf-offsets64.h"
+#include "savectx64.h"
 #include "sched.h"
 
+struct savectx exited;
+
+void print_test(){
+    int a;
+    printf("%x\n", &a);
+    for(a = 0; a < 2; a++)
+        printf("Hello world!\n");
+
+    restorectx(&exited, 0);
+}
+
 int main(int argc, char **argv){
-
+    sched_init(print_test);
 }
 
-sched_init(void (*init_fn)()){
-    /* call once by testbed program; init scheduling system
-    including setting up PIT (setitimer), est. sched_tick as
-    the signal for that timer, create initial task with pid 1
-    make it runnable and transfer execution to it (including
-    switching to its stack at location init_fn. Init_fn is not
-    expected to return and if it does so it is OK to have 
-    unpredictable results. */
+int sched_init(void (*init_fn)()){
+
+    // Set Interval Timer
+    struct itimerval itv_new;
+    itv_new.it_interval.tv_sec = 0;
+    itv_new.it_interval.tv_usec = IT_USEC;
+    itv_new.it_value.tv_sec = 0;
+    itv_new.it_value.tv_usec = IT_USEC;
+
+    if(setitimer(ITIMER_VIRTUAL, &itv_new, NULL) < 0){
+        fprintf(stderr, "Error: setitimer() failed: %s\n", strerror(errno));
+        exit(-1);
+    }
+
+    // Set Timer Handler to sched_tick
+    if(signal(SIGVTALRM, sched_tick) == SIG_ERR){
+        fprintf(stderr, "Error: signal() setting SIGVTALRM handler failed: %s\n", strerror(errno));
+        exit(-1);
+    }
+
+    // Initialize the pid_table
+    memset(pid_table, 0, sizeof pid_table);
+    pid_table[0] = 1; // 0 is not a valid free pid
+
+    // Create a new stack
+    void *sp_new;
+    if((sp_new = mmap(0, STACK_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0)) == MAP_FAILED){
+        fprintf(stderr, "Error: mmap() failed creating new stack for PID 1: %s\n", strerror(errno));
+        exit(-1);
+    }
+
+    // Create and initialize a sched_proc (task_struct)
+    struct sched_proc p_init;
+    p_init.task_state = SCHED_RUNNING;
+    p_init.ticks = 0;
+    p_init.priority = 0;
+    p_init.ppid = 1;
+    p_init.pid = 1; // could've also used sched_newpid()
+    pid_table[1] = 1;
+    p_init.sp_begin = sp_new;
+
+    // Create a new context and modify for the new process
+    struct savectx new;
+    savectx(&new);
+    new.regs[JB_BP] = sp_new+STACK_SIZE-8;
+    new.regs[JB_SP] = sp_new+STACK_SIZE-8;
+    new.regs[JB_PC] = init_fn;
+
+    *((int *)sp_new + 4) = 1;
+
+    printf("%x:%x\n", sp_new, sp_new+STACK_SIZE);
+
+    // Set the current pointer
+    current = &p_init;
+
+    // Save context to 'return' to label init_fn_returned
+    // Kinda like using longjmp
+    savectx(&exited);
+    exited.regs[JB_PC] = &&init_fn_returned; // GCC specific: && gets address of label
+
+    printf("ASHD\n");
+
+    // Pass execution to init_fn
+    restorectx(&new, 0);
+
+init_fn_returned:
+    fprintf(stderr, "Error: init_fn() returned! PANICCCCCC\n");
+    exit(-1);
 }
 
-sched_fork(){
+unsigned int sched_newpid(){
+    int i = 0;
+    for(; pid_table[i] && (i <= SCHED_NPROC); i++);
+    if(i > SCHED_NPROC) return 0; 
+    pid_table[i] = 1;
+    return i;
+}
+
+int sched_fork(){
     /* create a new simulated task which is a copy of the caller.
     Allocated a new pid for the child, and make runnable and eligible
     to be scheduled. it returns 0 to child and stuff. not required that
@@ -24,14 +112,14 @@ sched_fork(){
     private stack area and init to be a copy of the parent's */
 }
 
-sched_exit(int code){
+int sched_exit(int code){
     /* terminate current task, making it a ZOMBIE, and store the exit code.
     if a parent is sleeping in sched_wait(), wake it up and return the exit
     code of it. there will be no equivalent of SIGCHILD. sched_exit will not
     return. another runnable process will be scheduled. */
 }
 
-sched_wait(int *exit_code){
+int sched_wait(int *exit_code){
     /* return the exit code of a zombie child and free the resources of
     that child. if there is more tha one such child, the order in which
     the codes are returned is not defined. if none, but has at least one
@@ -41,20 +129,23 @@ sched_wait(int *exit_code){
     since no simulated signals, exit code is simply integer from sched_exit */
 }
 
-sched_nice(int niceval){
+int sched_nice(int niceval){
     /* set current task's nice value to param. from +19 to -20. clamp */
 }
 
-sched_getpid(){
+unsigned int sched_getpid(){
+    return current->pid;
 }
 
-sched_getppid(){
+unsigned int sched_getppid(){
+    return current->ppid;
 }
 
-sched_gettick(){
+unsigned int sched_gettick(){
+    return current->ticks;
 }
 
-sched_ps(){
+int sched_ps(){
     /* output to stderr a listing of all of the current tasks, including
     sleeping and zombie tasks. list the following information in tabular form:
         pid
@@ -70,14 +161,14 @@ sched_ps(){
     time by sending the testbed SIGABRT */
 }
 
-sched_switch(){
+int sched_switch(){
     /* never be called directly by the testbed. the only place where a
     context switch is made, analogous to schedule(). should place current
     on the run queue then select the best READY and put in RUNNING state
     and context switch */
 }
 
-sched_tick(){
+void sched_tick(int blah){
     /* sighandler for SIGVTALRM signal gen by the periodic timer
     examine currently running task and if its time slice just expired,
     mark that task as READY, place on run queue, and then call sched_switch()
--- 357/2016FA/ps08/sched.h
+++ 357/2016FA/ps08/sched.h
@@ -1,35 +1,39 @@
-struct sched_proc {
-    int task_state;
-    int ticks;
-    long long *stack_addr;
-    unsigned int priority;
-    int pid;
-    int ppid;
+#define SCHED_NPROC   1024
+#define SCHED_READY    001
+#define SCHED_RUNNING  002
+#define SCHED_SLEEPING 003
+#define SCHED_ZOMBIE   004
 
-    /* use this for each simulated task */
-    /* probably include task state, etc */
-    /* proiority, accumulated, cpu time, stack address, etc */
+#define STACK_SIZE    1024
+#define IT_USEC     100000
+
+struct sched_proc {
+    unsigned int task_state;
+    unsigned int ticks;
+    int nice;
+    unsigned int priority;
+    unsigned int pid;
+    unsigned int ppid;
+    void *sp_begin;
+    struct savectx ctx;
 };
 
 struct sched_waitq {
     /* use this for each event/wakeup queue */
 };
 
-#define SCHED_NPROC511
-#define SCHED_READY    001
-#define SCHED_RUNNING  002
-#define SCHED_SLEEPING 003
-#define SCHED_ZOMBIE   004
+struct sched_proc *current;
+unsigned short int pid_table[SCHED_NPROC + 1];
 
-sched_init(void (*init_fn)());
-sched_fork();
-sched_exit(int code);
-sched_wait(int *exit_code);
-sched_nice(int niceval);
-sched_getpid();
-sched_getppid();
-sched_gettick();
-sched_ps();
-sched_switch();
-sched_tick();
-
+int sched_init(void (*init_fn)());
+int sched_fork();
+int sched_exit(int code);
+int sched_wait(int *exit_code);
+int sched_nice(int niceval);
+unsigned int sched_getpid();
+unsigned int sched_getppid();
+unsigned int sched_gettick();
+int sched_ps();
+int sched_switch();
+void sched_tick(int);
+unsigned int sched_newpid();
